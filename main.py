c8726b1f679c3e64ed5a0e86dc2894a0c1a9db9a
DogsWillRule <74517672+mathurs23pen@users.noreply.github.com>
2022-06-06 23:14:38 -0400

    Added the options. Still haven't found an input method. Please give me a python library that you can use to get input from a mouse click that does not care about position. I mean, it should process the position, but it shouldn't use it as a parameter to execute the function like turtle does.

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def scoreboard():
     write(score, False, "center", ("arial", 20, "normal"))
 screen.addshape(image)
 shape(image)
+#Actually displaying stuff on the screen
 x=-300
 while x<600:
     pu()
@@ -44,8 +45,28 @@ questions = []
 questions.append(questionclass("Does Chad think Jason is the type of person to do drugs?", "Yes", "No", "A purple elephant", 2))
 questions.append(questionclass("Which character from Wonder is Anthony most similar to?", "Auggie", "Summer", "Julian", 3))
 questions.append(questionclass("Why would officer Costas always blame Chad for doing something he has not done?", "He hates Chad personally", "He thinks he is just like his dad (bad and always getting into trouble)", "he sees the worst in people without looking for actual evidence also known as “McCarthyism”", 3))
-questions.append(questionclass("Why is Manetti always so nice to Chad?", "Manetti is the good cop.", "He understands how Chad feels.", "He is Chad." 3))
+questions.append(questionclass("Why is Manetti always so nice to Chad?", "Manetti is the good cop.", "He understands how Chad feels.", "He is Chad.", 3))
 #Conflict
+questions.append(questionclass("Which of the following characters does Gwen go out with?", "Anthony", "Jason", "A purple elephant", 2))
+questions.append(questionclass("Why do you think Anthony decided to ask Gwen out? Select the option that can be proven using evidence from the book.", "Anthony decided to ask Gwen out because he knew it would annoy Chad. He may have also genuinely liked her, but if you consider how different he is from Gwen, that is probably not the case.", "Anthony decided to ask Gwen out because he needed a partner in crime and thought she would be easy to control.", "Anthony decided to ask Gwen out because Chad told him that they would go well together. He knew that Chad was a nice person, so he listened.", 1))
+questions.append(questionclass("How does Jason's sickness affect Chad? Select the answer that is not correct.", "Positively, Chad is able to practice being a Bozo and feel like he's helping Jason.", "Negatively, the fact that Chad felt like he was losing his best friend was part of the reason he went to the couch.", "Negatively. Because of this, Chad can't be a Bozo because he wanted to do it with Jason.", 3))
+questions.append(questionclass("How does Chad feel when Gwen tells him about the party? Why does he feel this way?", "Angry because he thinks Anthony has corrupted her. He views her as Little Miss Perfect and feels that being with Anthony would ruin her.", "Angry because he feels like he has been robbed of one of his possessions. He took a risk when he asked her to be his girlfriend and then she ran off with his nemesis.", "Guilty because he realizes that he should've asked her out before so that Anthony couldn't steal her from him.", 1))
+#Theme
+#questions.append(questionclass())
+#questions.append(questionclass())
+#questions.append(questionclass())
+#questions.append(questionclass())
+#Signpost
+#questions.append(questionclass())
+#questions.append(questionclass())
+#questions.append(questionclass())
+#questions.append(questionclass())
+#Plot
+#questions.append(questionclass())
+#questions.append(questionclass())
+#questions.append(questionclass())
+#questions.append(questionclass())
+#Function to display questions
 def questionfunc(questionnum):
     shuffle(questions)
     shape("square")
@@ -58,8 +79,19 @@ def questionfunc(questionnum):
     stamp()
     color("white")
     write(questions[questionnum].question, False, "center", ("Arial", 20, "normal"))
-pu()
-goto(0, 200)
-pd()
-screen.onkey(questionfunc(0), "space")
-screen.listen()
+    pu()
+    goto(-300, -150)
+    pd()
+    color("#2A0332")
+    stamp()
+    color("white")
+    write(questions[questionnum].choiceone, False, "center", ("Arial", 18, "normal"))
+    pu()
+    goto(0, -150)
+    pd()
+    write(questions[questionnum].choicetwo, False, "center", ("Arial", 18, "normal"))
+    pu()
+    goto(300, -150)
+    pd()
+    write(questions[questionnum].choicethree, False, "center", ("Arial", 18, "normal"))
+questionfunc(0)
